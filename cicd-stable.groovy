0bc003dda676bf3ec4d491a00fb3af7830d3d5c9
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:36:49 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/shdocport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/shdocport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/shdocport.git'), string(name: 'PORT_DESCRIPTION', value: 'shdoc' ), string(name: 'BUILD_LINE', value: 'STABLE')]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/shdocport.git'), string(name: 'PORT_DESCRIPTION', value: 'shdoc' ), string(name: 'BUILD_LINE', value: 'STABLE')]
   }
 }
